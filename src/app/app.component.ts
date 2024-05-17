203caa166907570c5b607e89a9801f25eb3dd371
Sofia <a25991@alunos.ipca.pt>
2024-05-17 15:17:26 +0100

    navegacao. O dropdown nao funciona

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,10 +10,16 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'TrabalhoWeb';
+  dropdown = false;
 
   traduzirPagina(){
     // codigo para traduzir pag
     // o alerta e so para confirmar que a funcao esta a funcionar
     alert('Page will be translated to English');
   }
+
+  toggleDropdown() {
+    this.dropdown = !this.dropdown;
+  }
+
 }
