4bc30a274279243a017dcdc59f8238bc31039914
Sofia <a25991@alunos.ipca.pt>
2024-05-18 21:42:20 +0100

    adicionar imagens para um museu

--- src/app/Museus/gerra-australia/gerra-australia.component.ts
+++ src/app/Museus/gerra-australia/gerra-australia.component.ts
@@ -9,19 +9,4 @@ import { Component } from '@angular/core';
 })
 export class GerraAustraliaComponent {
 
-  traduzirPagina(){
-    // codigo para traduzir pag
-    // o alerta e so para confirmar que a funcao esta a funcionar
-    alert('Page will be translated to English');
-  }
-  
-  SignUp(){
-    //codigo para sign up
-    alert("Sign up");
-  }
-
-  Login(){
-    //codigo para login
-    alert("Login");
-  }
 }
