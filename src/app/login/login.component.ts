bd272d8f779fe3b521376587206832d845e5c495
Daniela Pereira <a25988@alunos.ipca.pt>
2024-06-10 18:23:01 +0100

    login done

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -8,5 +9,17 @@ import { Component } from '@angular/core';
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  constructor(private router: Router) {}
 
-}
+  loginUser() {
+    // Lógica de autenticação aqui
+    // Após autenticação bem-sucedida, redirecionar para a página inicial
+    this.router.navigate(['/home']);
+  }
+
+  navigateToRegist() {
+    this.router.navigate(['/home']);
+  }
+
+  
+  }
