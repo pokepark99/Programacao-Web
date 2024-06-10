3ca3d051277a285d835c4f9e81b0e071d62d6e4c
Sofia <a25991@alunos.ipca.pt>
2024-06-10 16:02:10 +0100

    Module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,9 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
-
 import { AppComponent } from './app.component';
 import { InicialComponent } from './Inicial/inicial.component';
 import { GerraAustraliaComponent } from './Museus/gerra-australia/gerra-australia.component';
-//import { MelbourneComponent } from './Museus/melbourne/melbourne.component';
-//import { SouthWalesComponent } from './Museus/south-wales/south-wales.component';
 import { NationalAustralianComponent } from './Museus/national-australian/national-australian.component';
 import { LouvreComponent } from './Museus/louvre/louvre.component';
 import { LoginComponent } from './login/login.component';
@@ -18,8 +15,6 @@ import { routes } from './app.routes';
     AppComponent,
     InicialComponent,
     GerraAustraliaComponent,
-    //MelbourneComponent,
-   // SouthWalesComponent,
     NationalAustralianComponent,
     LouvreComponent,
     LoginComponent
