300f12ca887f039de6e9fdf04ad8fa34aa109bdc
Daniela Pereira <a25988@alunos.ipca.pt>
2024-06-11 00:45:20 +0100

    Translate

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,19 +1,40 @@
 import { Component } from '@angular/core';
+import { TranslateService, TranslateModule } from '@ngx-translate/core';
+import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+    HttpClientModule,
+    TranslateModule
+  ],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.css'
+  styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
+  searchPlaceholder: string = '';
 
+  constructor(private translate: TranslateService) {
+    // Define the default language
+    this.translate.setDefaultLang('en');
+    this.setPlaceholder();
 
-  traduzirPagina(){
-    // codigo para traduzir pag
-    // o alerta e so para confirmar que a funcao esta a funcionar
-    alert('Page will be translated to English');
+    // Subscribe to language changes to update the placeholder dynamically
+    this.translate.onLangChange.subscribe(() => {
+      this.setPlaceholder();
+    });
   }
-  
+
+  traduzirPagina(language: string) {
+    this.translate.use(language);
+  }
+
+  setPlaceholder() {
+    this.translate.get('SEARCH').subscribe((res: string) => {
+      this.searchPlaceholder = res;
+    });
+  }
 }
