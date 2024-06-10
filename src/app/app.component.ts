53176f45d363f38d7291009b7be128d79252cf09
Daniela Pereira <a25988@alunos.ipca.pt>
2024-06-10 21:24:21 +0100

    ++

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router, Event, NavigationEnd } from '@angular/router';
+import { filter } from 'rxjs/operators';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
@@ -8,10 +10,17 @@ import { FooterComponent } from './footer/footer.component';
   standalone: true,
   imports: [RouterOutlet, HeaderComponent, FooterComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'TrabalhoWeb';
+  showHeaderFooter = true;
 
+  constructor(private router: Router) {
+    this.router.events.pipe(
+      filter((event: Event): event is NavigationEnd => event instanceof NavigationEnd)
+    ).subscribe((event: NavigationEnd) => {
+      this.showHeaderFooter = !['/login', '/registe'].includes(event.urlAfterRedirects);
+    });
+  }
 }
-
