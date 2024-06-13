3a85d13484cac27937a4ebd57e598ca16006d80a
Daniela Pereira <a25988@alunos.ipca.pt>
2024-06-13 09:55:21 +0100

    Revert "j"
    This reverts commit a98fce7869df1d4c75043f0b8a8d511d01e496c3, reversing
    changes made to 3e7ce8457449ee65ea997265d8c5747fd9df2e37.

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
+<<<<<<< HEAD
 import { TranslateService, TranslateModule } from '@ngx-translate/core';
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
+=======
+>>>>>>> parent of a98fce7 (j)
 
 @Component({
   selector: 'app-header',
   standalone: true,
+<<<<<<< HEAD
   imports: [
     CommonModule,
     HttpClientModule,
@@ -36,5 +40,28 @@ export class HeaderComponent {
     this.translate.get('SEARCH').subscribe((res: string) => {
       this.searchPlaceholder = res;
     });
+=======
+  imports: [],
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.css'
+})
+export class HeaderComponent {
+
+
+  traduzirPagina(){
+    // codigo para traduzir pag
+    // o alerta e so para confirmar que a funcao esta a funcionar
+    alert('Page will be translated to English');
+  }
+  
+  SignUp(){
+    //codigo para sign up
+    alert("Sign up");
+  }
+
+  Login(){
+    //codigo para login
+    alert("Login");
+>>>>>>> parent of a98fce7 (j)
   }
 }
